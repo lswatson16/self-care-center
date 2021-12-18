72cf6f774f296484f6e902df8c8a14cb7afa0a52
lswatson16 <lauralynw16@gmail.com>
2021-12-18 17:48:32 -0500

    Move script tag

---
+++ main.js
@@ -0,0 +1,24 @@
+// query selectors
+
+
+
+var affirmations = [
+  'I forgive myself and set myself free.',
+  'I believe I can be all that I want to be.',
+  'I am in the process of becoming the best version of myself.'
+]
+
+var mantras = [
+  'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
+  'Don’t let yesterday take up too much of today.',
+  'Every day is a second chance.'
+]
+
+// create an instance of the class
+var quote = new Motivation({motivationInfo.category: 'affirmation', motivationInfo.message: 'I am powerful!'});
+
+// get a random number
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+
+// use random number as the index number for the selected array category
